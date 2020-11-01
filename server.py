e2bca71b44371d347362a911ac867f4c9acdb5eb
Robert Reutiman <rreutima@nd.edu>
2020-11-01 15:09:59 -0500

    CORS

--- server.py
+++ server.py
@@ -17,13 +17,19 @@ def start_service():
     dispatcher.connect('login', '/auth/', controller=fplController, action = 'AUTHENTICATE', conditions=dict(method=['POST']))
     dispatcher.connect('featured players', '/featured/', controller=fplController, action='GET_FEATURED', conditions=dict(method=['POST']))
 
+    # CORS related options connections
+    dispatcher.connect('movie_key_options', '/movies/:movie_id', controller=optionsController, action = 'OPTIONS', conditions=dict(method=['OPTIONS']))
+    dispatcher.connect('movie_options', '/movies/', controller=optionsController, action = 'OPTIONS', conditions=dict(method=['OPTIONS']))
+    dispatcher.connect('reset_key_options', '/reset/:movie_id', controller=optionsController, action = 'OPTIONS', conditions=dict(method=['OPTIONS']))
+    dispatcher.connect('reset_options', '/reset/', controller=optionsController, action = 'OPTIONS', conditions=dict(method=['OPTIONS']))
+    dispatcher.connect('rating_options', '/ratings/:movie_id', controller=optionsController, action = 'OPTIONS', conditions=dict(method=['OPTIONS']))
 
     conf = {
 
       'global': {
         'server.thread_pool': 5,
         'server.socket_host': '127.0.0.1',
-	      'server.socket_port': 53075,
+	      'server.socket_port': 3000,
 	    },
 
 	    '/': {
@@ -35,6 +41,19 @@ def start_service():
     app = cherrypy.tree.mount(None, config=conf)
     cherrypy.quickstart(app)
 
+# class for CORS
+class optionsController:
+    def OPTIONS(self, *args, **kwargs):
+        return ""
+
+# function for CORS
+def CORS(self):
+    cherrypy.response.headers["Access-Control-Allow-Origin"] = "*"
+    cherrypy.response.headers["Access-Control-Allow-Methods"] = "GET, PUT, POST, DELETE, OPTIONS"
+    cherrypy.response.headers["Access-Control-Allow-Credentials"] = "true"
+
+
 
 if __name__ == '__main__':
+    cherrypy.tools.CORS = cherrypy.Tool('before_finalize', CORS) # CORS
     start_service()
